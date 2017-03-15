a518ee86585c446b36cff11a5f72af8763840462
Eugene Lee <eugenelee@cpp.edu>
2017-03-14 23:31:59 -0700

    log error

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ function getData() {
         var items = result.rss.channel[0].item;
 
         for(var i = 0; i < items.length; i++) {
-          console.log(items[i].title[0], items[i].description[0]);
+          console.log(items[i].title[0], parseInt(items[i].description[0]));
 
           client.index({
             index: 'times',
