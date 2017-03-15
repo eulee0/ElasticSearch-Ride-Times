157a07f8820976b5c26b2928c4ad6230aad9aa36
Eugene Lee <eugenelee@cpp.edu>
2017-03-14 18:32:24 -0700

    index vs create

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ function getData() {
         for(var i = 0; i < items.length; i++) {
           console.log(items[i].title[0], items[i].description[0]);
 
-          client.index({
+          client.create({
             index: 'times',
             type: 'times',
             id: i,
